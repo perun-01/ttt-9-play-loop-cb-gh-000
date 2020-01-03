da762e0d1d14ca9ee767ce969b3c8322e452b6b7
Ranjeet Singh Sahni <singhranjeet5200@gmail.com>
2020-01-03 15:38:21 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,7 +37,7 @@ end
 
 # Define your play method below
 def play(board)
-  while board[location] != " " && board[location] != ""
+  while position_taken?(board, location) == true
     turn(board)
   end
 end
